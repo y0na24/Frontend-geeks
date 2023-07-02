7dda83d71a732b36008ff12372b9fd76bd348e4a
Матвей Клёнов <y0na24@mail.ru>
2023-07-02 16:40:20 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -12,11 +12,9 @@ const store = createStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
 )
